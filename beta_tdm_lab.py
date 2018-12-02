cb0e1c901e8d33c01f722521aaf53f17530d635a
elzimil <43863714+elzimil@users.noreply.github.com>
2018-12-03 01:22:49 +0300

    fix f

--- beta_tdm_lab.py
+++ beta_tdm_lab.py
@@ -30,13 +30,13 @@ def main():
     eta = (a**4 *density_water*g*K) / (2*L*d**2)
     print('eta:', eta)
     print('eta_err',eta - t_eta,'rel_eta_err',
-          abs(eta - t_eta)/t_eta)
+          100*abs(eta - t_eta)/t_eta)
     print('D: ', eta/density)
     
     lambd = 1.9 * eta * 5 / 2 * R / molarmass
     print('lambda: ', lambd)
     print('lambda_err',lambd - t_lambd,'rel_lambda_err',
-          abs(lambd - t_lambd)/t_lambd)
+          100*abs(lambd - t_lambd)/t_lambd)
 
     d_eff = ((4*molarmass*R*T)/(9*pi**3 * N_av**2 * eta**2))**(1/4)
     print('diam: ', d_eff)
@@ -46,6 +46,6 @@ def main():
     print('diam_o2: ',d_eff_o2,'diam_n2:',d_eff_n2)
     print('diam_o2_error:', (d_eff_o2 - t_d_eff_o2),
           'diam_n2_error:', (d_eff_n2 - t_d_eff_n2))
-    print('rel_diam_o2_error:', abs((d_eff_o2 - t_d_eff_o2)/t_d_eff_o2),
-          'rel_diam_n2_error:', abs((d_eff_n2 - t_d_eff_n2)/t_d_eff_n2))
+    print('rel_diam_o2_error:', 100*abs((d_eff_o2 - t_d_eff_o2)/t_d_eff_o2),
+          'rel_diam_n2_error:', 100*abs((d_eff_n2 - t_d_eff_n2)/t_d_eff_n2))
 main()
